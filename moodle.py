4ef5b8d989986223af41db698f11edb4c729d813
Martin Vuk <martin.vuk@fri.uni-lj.si>
2019-04-16 09:02:19 +0200

    moodle user

--- moodle.py
+++ moodle.py
@@ -16,14 +16,22 @@ def lang_format(self, data):
                     new_value += template.format(lang, value[lang])
         return new_value    
                 
-def user(s_user, auth="ldap"):
-    return mdl.User( username = s_user['upn'],
+def user(s_user, auth="ldap", moodle_courses=None):
+    if s_user.get('predmetnik'):
+        course_idnumbers = [c['sifra_predmeta'] for c in s_user.get('predmetnik')]
+    else:
+        course_idnumbers = []
+    user = mdl.User( username = s_user['upn'].strip(),
             firstname = s_user['ime'],
             lastname = s_user['priimek'],
             email = s_user['email'],
             auth = auth,
-            idnumber = s_user.get('vpisna_stevilka')
-            password = 'Geslo je t4ko doloceno z ldap' )
+            idnumber = s_user.get('vpisna_stevilka'),
+            password = 'Geslo je t4ko doloceno z ldap',
+            course_idnumbers = course_idnumbers)
+    if moodle_courses:
+        user.enrolments(moodle_courses)
+    return user
 
 def course(s_course):
     return mdl.Course(fullname = lang_format(s_course['naslov']),
@@ -36,8 +44,8 @@ def cathegory(s_tree):
             parent_idnumber = s_tree['parent'],
             idnumber = s_tree['id'])
 
-def student_enrolments(student, m_courses, s_courses):
-    pass
+
+    
 
 def teacher_enrolments(course, m_courses, s_courses, s_teachers):
     pass
